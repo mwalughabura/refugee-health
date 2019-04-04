fb26bddc1dfb559be38c0d7b80321bd89a45bc8b
Habiba juma <habiba.amisa@gmail.com>
2019-04-04 18:44:27 +0300

    Added a signup4 page

---
+++ src/app/signup4/signup4.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { Signup4Page } from './signup4.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Signup4Page
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [Signup4Page]
+})
+export class Signup4PageModule {}
---
+++ src/app/signup4/signup4.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup4',
+  templateUrl: './signup4.page.html',
+  styleUrls: ['./signup4.page.scss'],
+})
+export class Signup4Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
